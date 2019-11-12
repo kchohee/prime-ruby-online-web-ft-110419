7d936fc5e86ce6ffc14254feaeb8a7fe07169d64
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-12 03:45:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,3 +2,6 @@
 # prime number is a number thats divisible by only itself and 1
 #start at 2 , go until number and see if any n is divisible
 #ex: if number 5 , then start 2 is divisible?, then 3 is divisible?, then 4 is divisible?
+# any negative number is not prime, 0 is not prime, 1 is not prime
+def prime?(num)
+  puts [1..6]
