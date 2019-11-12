76a30386d2e133ecbee37dfeb5121e850e76ea21
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-12 03:40:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,4 @@
 # Add  code here!
+# prime number is a number thats divisible by only itself and 1
+#start at 2 , go until number and see if any n is divisible
+#ex: if number 5 , then start 2 is divisible?, then 3 is divisible?, then 4 is divisible?
